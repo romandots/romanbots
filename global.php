08d29add26ad95ba3116ca1cf63ddb7bc2cd303b
romandots <ram.d.kreiz@gmail.com>
2018-06-15 01:56:24 +0300

    Add config helper functions Fix memory error with print_r recursion

--- global.php
+++ global.php
@@ -1,6 +1,7 @@
 <?php
 
-use RomanBots\Human\Human;
+use RomanBots\Bots\Human;
+use RomanBots\Bots\VkBot;
 
 require_once "lib/Redis.php";
 
@@ -9,7 +10,7 @@ require_once "lib/Redis.php";
  * @param $message string
  */
 function _debug_output($message){
-	// $bot = new RomanBots\VkBot\VkBot();
+	// $bot = new VkBot();
 	// $bot->human = Human::fromVk( TESTER_UID);
 	// $bot->reply($message."\n\n");
 }
@@ -18,21 +19,38 @@ function _debug_log($message){
 	$trace = debug_backtrace();
 	$function_name = isset($trace[2]) ? $trace[2]['function'] : '-';
 	$mark = date("H:i:s") . ' [' . $function_name . ']';
-	if(!is_dir(BOT_BASE_DIRECTORY.'/logs')){
-		@mkdir(BOT_BASE_DIRECTORY.'/logs');
-		@chmod( BOT_BASE_DIRECTORY.'/logs', 0777);
+	if(!is_dir( BASE_DIR.'/logs')){
+		@mkdir( BASE_DIR.'/logs');
+		@chmod( BASE_DIR.'/logs', 0777);
 	}
-	$log_name = BOT_BASE_DIRECTORY.'/logs/' . date("Y-M-D") . '.txt';
+	$log_name = BASE_DIR.'/logs/' . date( "Y-m-d") . '.txt';
 	@file_put_contents($log_name, $mark . " : " . $message . "\n\n", FILE_APPEND);
 }
 
 function _debug($message){
-	if(DEBUG && TESTER_UID){
-		_debug_output($message);
+	if(DEBUG ){
+		if(TESTER_UID){
+			_debug_output($message);
+		}
+		dump($message);
 	}
 	if(LOG){
 		_debug_log($message);
 	}
+}
+
+function dump($var, $comment = ''){
+	if($comment) {
+		echo  "<h4>$comment</h4>";
+	}
+	echo  "<pre>";
+	var_dump($var);
+	echo  "</pre>";
+}
+
+function dd($var, $comment = ''){
+	dump($var, $comment);
+	exit();
 }
 
 function debug($var, $comment = ''){
@@ -42,7 +60,7 @@ function debug($var, $comment = ''){
 	}
 	ob_start();
 	var_dump($var);
-	$message .= ob_get_clean();
+	$message .= ob_get_flush();
 	_debug($message);
 }
 
@@ -64,4 +82,39 @@ function log_error($message) {
 function fatal($message){
 	log_error($message);
 	die('ok');
+}
+
+function _config_get_data($array, $path, $default = null)
+{
+	if(is_array($array))
+	{
+		$item = array_shift($path);
+		if(key_exists($item, $array))
+		{
+			$var = $array[ $item ];
+			if(count($path))
+			{
+				return _config_get_data($var, $path, $default);
+			} else {
+				return $var;
+			}
+		}
+	} else {
+		return $array;
+	}
+}
+
+function config($var, $default = null) {
+	if( $path = explode('.', $var) ){
+		$filename = BASE_DIR.'/config/'.array_shift($path).'.php';
+		if(file_exists($filename))
+		{
+			$config = include($filename);
+			$data = _config_get_data($config, $path, $default);
+			debug($data, "Loading config var: $var");
+			return $data;
+		}
+	}
+	debug($default, "Loading config var: $var - failed, returning default value:");
+	return $default;
 }
